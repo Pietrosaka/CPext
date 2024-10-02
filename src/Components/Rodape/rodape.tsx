9b55dea10be00dc00f918b9b1c2582399bfa6607
IanMadeira <ian.madeira17@gmail.com>
2024-10-01 23:16:40 -0300

    Dados integrante Ian adicionados

--- src/Components/Rodape/rodape.tsx
+++ src/Components/Rodape/rodape.tsx
@@ -1,6 +1,7 @@
 <footer className="bg-gray-900 text-white py-6 mt-8">
   <div className="container mx-auto text-center">
     <p>Integrantes</p>
+    <p>Ian Madeira - RM 555502</p>
     <p>Pietro Saccarrão Cougo - RM 555839</p>
     <p>&copy; 2024 Meu Projeto. Todos os direitos reservados.</p>
   </div>
