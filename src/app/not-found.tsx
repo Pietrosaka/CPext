014ac337cc81f5492d9ed3653caa93ae420d362c
Pietrosaka <rm555839@fiap.com.br>
2024-10-01 22:03:25 -0300

    imagem de erro funcionando

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,27 @@
-export default function NotFound() {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-4xl text-red-500">404 - Página não encontrada</h1>
-      </div>
-    );
-  }
-  
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Página não encontrada</h1>
+      <p>A página que você está tentando acessar não existe.</p>
+
+      {/* Componente Image do Next.js */}
+      <Image
+        src="/img-404.jpg"
+        alt="Erro 404"
+        width={500}
+        height={300}
+      />
+
+      <p>
+        <Link href="/">
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
